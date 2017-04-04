67b38f3d86df6875404e674b5c717fb460683597
Ankur Mishra <ankur.reborn@gmail.com>
2017-04-05 02:41:50 +0530

    modifying db for postgresql db on heroku server

--- src/db.js
+++ src/db.js
@@ -2,10 +2,19 @@
  * Created by ankurmishra on 4/5/17.
  */
 var Sequelize = require("sequelize");
-var sequelize = new Sequelize(undefined,undefined,undefined, {
-    "dialect": "sqlite",
-    "storage": "../data/msg-api-db.sqlite"
-});
+var env = process.env.NODE_ENV || "development";
+var sequelize;
+
+if (env==="production"){
+    sequelize = new Sequelize(process.env.DATABASE_URL,{
+        "dialect": "postgres"
+    });
+}else{
+    sequelize = new Sequelize(undefined,undefined,undefined, {
+        "dialect": "sqlite",
+        "storage": "../data/msg-api-db.sqlite"
+    });
+}
 
 var db = {};
 db.message = sequelize.import("../models/msg.js");
